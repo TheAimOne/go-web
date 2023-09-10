c62f16211642a024b9f83e602530ad75ad192696
sandeep poomanglaath <sandeeppoomanglaath@sandeeps-MacBook-Pro.local>
2023-09-10 21:40:32 +0800

    update mocks

--- pkg/repository/event_test.go
+++ pkg/repository/event_test.go
@@ -1,1 +1,30 @@
 package repository
+
+import (
+	"testing"
+
+	mock "github.com/go-web/mocks/database/function"
+)
+
+var dbFunctionMock *mock.DBFunction
+var eventRepository EventRepository
+
+func init() {
+	dbFunctionMock = new(mock.DBFunction)
+	eventRepository = NewEventRepository(dbFunctionMock)
+}
+
+func TestCreateEvent(t *testing.T) {
+	t.Run("DB Error", func(t *testing.T) {
+
+	})
+
+	t.Run("DB Success", func(t *testing.T) {
+
+	})
+
+}
+
+func TestGetEventsByGroupId(t *testing.T) {
+
+}
